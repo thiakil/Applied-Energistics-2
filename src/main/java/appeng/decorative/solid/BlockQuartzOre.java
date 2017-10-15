b6b63d59c8b6c98d9908834d0a8ab11d5d883584
Xander <x@xandervictory.id.au>
2017-10-15 13:15:28 +0800

    fix `Block.getItem` being wrong for charged certus ore

--- src/main/java/appeng/decorative/solid/BlockQuartzOre.java
+++ src/main/java/appeng/decorative/solid/BlockQuartzOre.java
@@ -24,6 +24,7 @@ import java.util.Random;
 import net.minecraft.block.material.Material;
 import net.minecraft.block.state.IBlockState;
 import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
 import net.minecraft.util.BlockRenderLayer;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.MathHelper;
@@ -151,5 +152,12 @@ public class BlockQuartzOre extends AEBaseBlock
 	public void setEnhanceBrightness( final boolean enhanceBrightness )
 	{
 		this.enhanceBrightness = enhanceBrightness;
+	}
+
+	@Override
+	@SuppressWarnings( "deprecation" )
+	public ItemStack getItem( World worldIn, BlockPos pos, IBlockState state )
+	{
+		return new ItemStack(Item.getItemFromBlock(this), 1, 0);
 	}
 }
